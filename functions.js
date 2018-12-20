d00c639508a9318ce69d477b247e055f6ebe6135
Tuptewar <Pavan.Tuptewar@mastercard.com>
2018-12-20 17:47:46 +0530

    First Setup

---
+++ functions.js
@@ -0,0 +1,10 @@
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 65:
+            //document.getElementById('B').play();
+            console.log("Key is not found!");
+            break;
+        default:
+            console.log("Key is not found!");
+    }
+};
--- main.js
+++ main.js
@@ -7,10 +7,13 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600,
+  backgroundColor: "#c0bdff"})
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+
+  // mainWindow.setMenu(null);
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -20,10 +23,32 @@ function createWindow () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+  mainWindow = null
+
+  
   })
 }
 
+//Handle to create window
+function createAddWindow(){
+  // Create the browser window.
+  window2 = new BrowserWindow({width: 800, height: 600,
+  backgroundColor: "#c0bdff"})
+
+  // and load the index.html of the app.
+  window2.loadFile('booklist.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  window2.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    window2 = null
+  })
+}
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
